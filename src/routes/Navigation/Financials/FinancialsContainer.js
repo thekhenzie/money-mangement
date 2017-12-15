f071843eaab2a231d84c0d78014b302da5a56597
Harold Khen Baniqued <hbaniqued@blastasia.com>
2017-12-15 17:58:24 +0800

    december 15 commit

--- src/routes/Navigation/Financials/FinancialsContainer.js
+++ src/routes/Navigation/Financials/FinancialsContainer.js
@@ -12,58 +12,294 @@ import {
 
 import { RoutedComponent, connect } from 'routes/routedComponent';
 import { CONTENT_VIEW_STATIC } from 'layouts/DefaultLayout/modules/layout';
-
+import { retrieveLoggedUser } from '../../../auth';
+import NotificationSystem from 'react-notification-system';
+import { toastr } from '../../../utils/toastr';
 //import classes from './../Pages.scss';
-
+var ctr = 0;
 class FinancialsContainer extends RoutedComponent {
     constructor(props) {
         super(props);
 
         this.state = {
-            input: {
+            statementInput: {
                 income: 0.0
             },
+            selectedStatement: {},
+            billsInput: [],
+            selectedBills: [],
+            selectedBillsRow: [],
             errors: {},
             isEditing: false,
             isBillEditing: false,
-            isAdding: false
+            isAdding: false,
+            errors: {}
         };
+
+        this.onTextChange = this.onTextChange.bind(this);
+        this.onSaveStatement = this.onSaveStatement.bind(this);
+        this.updateStatement = this.updateStatement.bind(this);
+        this.onAddRow = this.onAddRow.bind(this);
+        this.onRemoveRow = this.onRemoveRow.bind(this);
+        this.onTextChangeBills = this.onTextChangeBills.bind(this);
+    }
+    componentDidMount() {
+        this.retrieveStatement();
+    }
+    retrieveStatement() {
+        debugger;
+        let loggedUser = retrieveLoggedUser();
+        let year = new Date().getFullYear();
+        let month = new Date().getMonth() + 1;
+
+        return fetch(`http://artlertestingapi.azurewebsites.net/api/Statements?adminID=31`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${loggedUser.token}`
+            }
+        })
+            .then(response => response.json())
+            .then(responseJSON => {
+                debugger;
+                let selectedStatement = responseJSON.result.find(x => {
+                    return x.month == month && x.year == year && x.userID == loggedUser.id;
+                });
+                this.setState({ selectedStatement });
+                this.retrieveBill();
+                // console.log(selectedStatement);
+                // selectedStatement == undefined ? this.createStatement() : this.setState({ selectedStatement });
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+    createStatement() {
+        debugger;
+        let loggedUser = retrieveLoggedUser();
+        let year = new Date().getFullYear();
+        let month = new Date().getMonth();
+        return fetch(`http://artlertestingapi.azurewebsites.net/api/Statements`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${loggedUser.token}`
+            },
+            body: JSON.stringify({
+                userID: Number(loggedUser.id),
+                income: 0,
+                month: Number(month),
+                year: Number(year)
+            })
+        })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                this.setState({ selectedStatement: responseJSON.result });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+    updateStatement() {
+        debugger;
+        let loggedUser = retrieveLoggedUser();
+        let year = new Date().getFullYear();
+        let month = new Date().getMonth();
+        return fetch(`http://artlertestingapi.azurewebsites.net/api/Statements/${this.state.selectedStatement.statementID}`, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${loggedUser.token}`
+            },
+            body: JSON.stringify({
+                statementID: this.state.selectedStatement.statementID,
+                userID: Number(loggedUser.id),
+                income: this.state.statementInput.income,
+                month: Number(month),
+                year: Number(year)
+            })
+        })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                this.setState({
+                    isEditing: false,
+                    selectedRuleSet: responseJSON.result
+                });
+                toastr('Income updated', 'success', this.refs.notificationSystem);
+                setTimeout(() => {
+                }, 500);
+            })
+            .catch(error => {
+                toastr(error.message, 'error', this.refs.notificationSystem);
+                console.log(error);
+            });
+    }
+    retrieveBill() {
+        debugger;
+        let loggedUser = retrieveLoggedUser();
+        let year = new Date().getFullYear();
+        let month = new Date().getMonth() + 1;
+
+        return fetch(`http://artlertestingapi.azurewebsites.net/api/Bills/GetByStatementID/${this.state.selectedStatement.statementID}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${loggedUser.token}`
+            }
+        })
+            .then(response => response.json())
+            .then(responseJSON => {
+                this.setState({ selectedBills: responseJSON.result });
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+    createBill() {
+        let loggedUser = retrieveLoggedUser();
+        return fetch(`http://artlertestingapi.azurewebsites.net/api/Bills/PostMany`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `bearer ${loggedUser.token}`
+            },
+            body: JSON.stringify([
+                {
+                    userID: Number(loggedUser.id),
+                    income: 0,
+                    month: Number(month),
+                    year: Number(year)
+                }
+            ])
+        })
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                this.setState({ selectedStatement: responseJSON.result });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
     onTextChange(event) {
-        let input = Object.assign({}, this.state.input);
-        input[event.target.name] = event.target.value;
-        this.setState({ input });
+        let billsInput = Object.assign({}, this.state.billsInput);
+        billsInput[event.target.name] = event.target.value;
+        this.setState({ statementInput });
+    }
+    onTextChangeBills(event) {
+        debugger;
+        let statementInput = Object.assign({}, this.state.statementInput);
+        statementInput[event.target.name] = event.target.value;
+        this.setState({ statementInput });
+    }
+    onSaveStatement() {
+        debugger;
+        if (this.handleValidation(false)) {
+            this.updateStatement();
+        } else {
+            toastr('Please fill all fields properly', 'error', this.refs.notificationSystem);
+        }
+    }
+    handleValidation(isEditing) {
+        debugger;
+        let statementInput = isEditing ? this.state.selectedStatement : this.state.statementInput;
+        let errors = this.state.errors;
+        errors["income"] = "";
+
+
+        let formIsValid = true;
+
+        //Income
+        if (!statementInput.income) {
+            formIsValid = false;
+            errors["income"] = "Income cannot be empty ";
+
+            this.setState({ errors: errors });
+            return formIsValid;
+        }
+    }
+    onAddRow() {
+        ctr += 1;
+        // this.setState({ isAdding: true });
+        let selectedBillsRow = this.state.selectedBillsRow;
+        selectedBillsRow.push(<tr key={ctr}>
+            <td><FormControl type='text' name='name' required /></td>
+            <td className='v-a-m' id={ctr}>
+                <FormGroup className='m-b-0'>
+                    <InputGroup>
+                        <InputGroup.Addon>$</InputGroup.Addon>
+                        <FormControl type="text" name='amount' onChange={this.onTextChangeBills} />
+                    </InputGroup>
+                </FormGroup>
+            </td>
+            <td id={ctr}>
+                &nbsp;<i className='fa fa-close' id={ctr} onClick={this.onRemoveRow}></i>
+            </td>
+        </tr>);
+        this.setState({ selectedBillsRow });
+    }
+    onRemoveRow(event) {
+        let selectedBillsRow = this.state.selectedBillsRow;
+        let index = selectedBillsRow.findIndex(k => k.key == event.target.id);
+        selectedBillsRow.splice(index, 1);
+        this.setState({ selectedBillsRow });
     }
     getLayoutOptions() {
         return {
             contentView: CONTENT_VIEW_STATIC
         }
     }
-
     render() {
+        let rows = []
+        this.state.selectedBills[0] != null ?
+            this.state.selectedBills.map(r => {
+                rows.push(<tr>
+                    <td className='text-white'>{r.name}</td>
+                    <td className='text-white'>$ {r.amount}</td>
+                </tr>)
+            }) : <tr><td>No bills yet</td><td></td></tr>;
+
         return (
             <Row>
                 <Col lg={8}>
                     {this.state.isEditing ?
                         <div className='pull-right' style={{ zIndex: '2', position: 'relative', cursor: 'pointer' }}>
-                            <Button bsStyle='success' className='m-r-2' onClick={() => this.onSave()}><i className='fa fa-save' /> Save</Button>
+                            <Button bsStyle='success' className='m-r-2' onClick={() => this.updateStatement()}><i className='fa fa-save' /> Save</Button>
                             <a className='text-danger' onClick={() => this.setState({ isEditing: false, errors: {} })}>
                                 <i className='fa fa-ban' /> Cancel
                         </a>
                         </div> :
                         <div className='pull-right' style={{ zIndex: '2', position: 'relative', cursor: 'pointer' }}>
-                            <a onClick={() => this.setState({ isEditing: true })}>
+                            <a onClick={() => this.setState({ isEditing: true, statementInput: { income: this.state.selectedStatement.income } })}>
                                 <i className='fa fa-pencil'></i> Edit
                         </a>
                         </div>
                     }
+
                     <Divider >
                         <div className='text-white'>Your Net Income</div>
                     </Divider>
-                    <h1 className='text-mint-green'>$1,568</h1>
+                    {this.state.isEditing ?
+                        <div>
+                            <div className={this.state.errors.income ? "form-group has-error has-feedback" : "form-group "}>
+                                <FormGroup className='m-b-0'>
+                                    <FormControl type='number' step='any' min='0' placeholder='Input %' name='income' onChange={this.onTextChange} value={this.state.statementInput.income} required />
+                                </FormGroup>
+                            </div>
+                            <span className="label label-danger">{this.state.errors.income}</span>
+                            <br />
+                        </div>
+                        : <h1 className='text-mint-green'>$ {this.state.selectedStatement.income == undefined ? '0' : this.state.selectedStatement.income}</h1>
+                    }
+
+
                     <hr />
                     <div className='text-white'>YOUR TOTAL BILLS</div><br />
-                    <Button bsStyle='primary' onClick={() => this.setState({ isAdding: true })}>Add bill</Button>
+                    <Button bsStyle='primary' onClick={() => this.onAddRow()}>Add bill</Button>
                     <Row>
                         <Col lg={12}>
                             <br />
@@ -99,52 +335,16 @@ class FinancialsContainer extends RoutedComponent {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Cellphone</td>
-                                <td className='text-white v-a-m'>
-                                    {/* {this.state.isEditingLegend ?
-                                        <div>
-                                            <div className={this.state.errors.lowerLimit ? "form-group has-error has-feedback" : "form-group "}>
-                                                <FormControl type='number' step='any' min='0' placeholder='Input %' name='lowerLimit' onChange={this.onTextChangeEdit} value={this.state.selectedRuleSetInput.lowerLimit} required />
-                                            </div>
-                                            <span className="label label-danger">{this.state.errors.lowerLimit}</span>
-                                        </div> :
-                                        <div>&lt;{this.state.selectedRuleSet.lowerLimit} %</div>
-                                    } */}
-                                    $40.00
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Rent</td>
-                                <td className='text-white v-a-m'>
-                                    {/* {this.state.isEditingLegend ?
-                                        <div>{this.state.selectedRuleSetInput.lowerLimit}-{this.state.selectedRuleSetInput.upperLimit}%</div>
-                                        :
-                                        <div>{this.state.selectedRuleSet.lowerLimit}-{this.state.selectedRuleSet.upperLimit}%</div>
-                                    } */}
-                                    $400.00
-                                </td>
-                            </tr>
+                            {rows}
+                            {this.state.selectedBillsRow}
                             {this.state.isAdding ?
-                                <tr>
-                                    <td><FormControl type='text' required /></td>
-                                    <td className='v-a-m'>
-                                        <FormGroup className='m-b-0'>
-                                            <InputGroup>
-                                                <InputGroup.Addon>$</InputGroup.Addon>
-                                                <FormControl type="text" />
-                                            </InputGroup>
-                                        </FormGroup>
-                                    </td>
-                                    <td>
-                                        &nbsp;<i className='fa fa-close' onClick={() => this.setState({ isAdding: false })}></i>
-                                    </td>
-                                </tr>
+                                null
                                 : null}
                         </tbody>
                     </Table>
                     <Button bsStyle='success'>Save New Bill</Button>
                 </Col>
+                <NotificationSystem ref="notificationSystem" />
             </Row>
         );
     }
